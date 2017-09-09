cc52e0b64e0584d9a5fd0cb7a7e379c22ea11d2b
Ahmed Selim <31737431+Ahmed-Selim@users.noreply.github.com>
2017-09-09 13:52:23 +0200

    Add files via upload

---
+++ A7med.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned int ui ;
+typedef unsigned long int ul ;
+typedef long long int ll ;
+typedef unsigned long long int ull ;
+typedef complex<double> point ;
+
+#define endl '\n'
+#define floop(i,q,w) 	for(int (i)=int(q) ; (i) < (int)(w) ; ++(i) )
+#define itloop(i,v)     for( i = begin(v) ; i != end(v) ; ++i)
+#define itrange(v)		begin(v),end(v)
+/**** Geometry ****/
+#define X real()
+#define Y imag()
+#define Polar (r,t)		((r)*exp(point(0,(t))))
+#define Length(a)		hypot((a).X,(a).Y)
+#define Angle(a)		aton2((a).Y,(a).X)
+#define Vec(a,b)		((b)-(a))
+#define DotProd(a,b)	((conj(a)*(b)).real())
+#define CrossProd(a,b)	((conj(a)*(b)).imag())
+#define LengthSqr(a)	DotProd(a,a)
+#define Rotate(v,t)		((v)*exp(point(0,t)))
+#define RotateAbout(v,t,a)	(Rotate(Vec(a,v),t)+(a))
+#define Reflect(v,m)	(conj((v)/(m))*(m))
+
+const int OO = (int) 1e9;
+const double PI = 2 * acos(0.0);
+const double eps = 1e-9;
+
+/***    Short = 3e4 , Integer = 2e9 , Long = 9e18 ***/
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r" , stdin);
+	//freopen("output.txt", "w", stdout);
+#endif
+
+	// Your Code Go Here :D
+
+	return 0;
+}
